c945b97bbc9352c08353018d63003e567f987efb
ZhengPengqiao <157510312@qq.com>
2016-09-30 16:42:40 +0800

    value = (TH1 << 8) + TL1;  //+的优先级比<<大.所以应将(TH1 << 8)括起来,

--- main.c
+++ main.c
@@ -8,18 +8,28 @@ void main()
 {
 
 	char a=0;
+	char f = 0;
 	unsigned int value;
 	initLcd();
     while(1)
     {
-		sh[4] = a + '0';
-		sh[10] = a + '0';
+		if(f == 0)
+		{
+			sh[4] = a + '0';
+			sh[10] = a + '0';
+			TH1 = a;
+			TL1 = a;
+		}
+		else
+		{
+			sh[4] = a + '0';
+			sh[10] = '0';
+			TH1 = a;
+			TL1 = 0;
+		}
 		LcdShowString(0,0,sh);
-		TH1 = a;
-		TL1 = a;
-		delay10usValue(50000);
-		value = TH1 * 256 + TL1;
 		
+		value = TH1 * 256 + TL1;
 		data[5] = value % 10 + '0';
 		data[4] = value /10 % 10 + '0';
 		data[3] = value /100% 10 + '0';
@@ -30,7 +40,8 @@ void main()
 		LcdShowString(0,1,data);
 		
 		
-		value = TH1 << 8 + TL1;
+		value = (TH1 << 8) + TL1;  //+的优先级比<<大.所以应将(TH1 << 8)括起来,
+									//不然会先＋然后在移位,这样就会出错
 		
 		data[5] = value % 10 + '0';
 		data[4] = value /10 % 10 + '0';
@@ -46,9 +57,20 @@ void main()
 		delay10usValue(50000);
 		delay10usValue(50000);
 		a++;
+		
 		if(a > 10)
 		{
 			a = 0;
 		}
+		
+		if(f == 0 && a == 0)
+		{
+			f = 1;
+		}
+		else if(f == 1 && a == 0)
+		{
+			f = 0;
+		}
+	
     }
 }
