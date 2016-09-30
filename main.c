c1841a01d0ba3816568188723c3630bf808b70c8
ZhengPengqiao <157510312@qq.com>
2016-09-30 23:06:51 +0800

    I2C接口相关的API基本实现

--- main.c
+++ main.c
@@ -1,26 +1,52 @@
 #include "TIMER.h"
 #include "LEDSHU.h"
-#include "LED.h"
-#include "HS0038.h"
+#include "LCD1602.h"
+#include "I2C.h"
 
 __xdata unsigned char data[6] = {0,0,0,0,0,0};
+
+unsigned i2cAddressing(unsigned char addr)
+{
+	char ack;
+	i2cStart();
+	ack = i2cWrite(addr << 1);
+	i2cStop();
+	return ack;
+}
+
 
 void main()
 {
 
-	hs0038Init();
+	unsigned char ack;
 	
+	__xdata char str[10];
+	initLcd();
+
+
     while(1)
     {   
-		if(irflag)
-		{
-			irflag = 0;
-			data[5] = ircode[0] >> 4;
-			data[4] = ircode[0] & 0x0F;
-			data[1] = ircode[2] >> 4;
-			data[0] = ircode[2]&0x0F;
-		}
-		updateLedShu(data);
-		updateLed();
+		ack = i2cAddressing(0x50);
+		str[0] = '5';
+		str[1] = '0';
+		str[2] = ':';
+		str[3] = ack + '0';
+		str[4] = '\0';
+		LcdShowString(0,1,str);
+
+
+		ack = i2cAddressing(0x62);
+		str[0] = '6';
+		str[1] = '2';
+		str[2] = ':';
+		str[3] = ack + '0';
+		str[4] = '\0';
+		LcdShowString(0,0,str);
+		
+		
+		delay10usValue(50000);
+		delay10usValue(50000);
+		delay10usValue(50000);
+		
     }
 }
