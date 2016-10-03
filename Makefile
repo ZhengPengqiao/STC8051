1bb28988482f9b65cca3bb337d0282d513abb572
ZhengPengqiao <157510312@qq.com>
2016-10-03 10:22:25 +0800

    综合项目1

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 #找到所有的源文件
-SRC := main.c  ./TIMER/TIMER.c 	./I2C/I2C.c ./LCD1602/LCD1602.c \
-		./PCF8591/PCF8591.c
+SRC := main.c  ./TIMER/TIMER.c 	./LED/LED.c ./LEDZHEN/LEDZHEN.c ./LEDSHU/LEDSHU.c
 #找到所有的头文件路径
 INCLUDE := $(shell find ./ -name "*.h")
 # dir : 取目录函数
--- main.c
+++ main.c
@@ -1,45 +1,54 @@
+#include "LED.h"
 #include "TIMER.h"
-#include "LCD1602.h"
-#include "DS1302.h"
-#include "PCF8591.h"
+#include "LEDSHU.h"
+#include "LEDZHEN.h"
 
+__xdata unsigned char image[] = {
+0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+0x7D,0x01,0x01,0x7D,0xFF,0xFF,0xE3,0xC1,
+0x81,0x03,0x03,0x81,0xC1,0xE3,0xFF,0xFF,
+0x81,0x01,0x3F,0x3F,0x3F,0x01,0x81,0xFF,
+0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+};
+signed char data[] = {0,0,0,0,0,0};
+unsigned char index = 0;
+void zhenFun()
+{
+		index ++;
+		if(index > 32)
+		{
+				index = 0;
+		}
+}
+
+
+
+void ledFun()
+{
+	setAllLedToggle();
+}
+
+void timer2Fun()
+{
+		static unsigned char i = 0;
+		switch(i)
+		{
+				case 0:updateLedZhen(image+index); i = 1;break;
+				case 1:updateLedShu(data);i = 2;break;
+				case 2:updateLed();i = 0; break;
+		}
+	   
+}
 void main()
 {
-	unsigned char val = 1;
-	unsigned char retval;
-	unsigned char str[16];
-	initLcd();
-
-	while (1)
+	int i = 0;
+	addTIMER0Task(200,zhenFun,1);
+	addTIMER0Task(1000,ledFun,2);
+	   
+	initTimer2(500);
+	setTimer2Fun(timer2Fun);
+	while(1)
 	{
-		//设置DA的数值
-		setDACValue(val);
-		str[0] = val / 100 % 10 + '0';
-		str[1] = val / 10 % 10 + '0';
-		str[2] = val     % 10 + '0';
-		str[3] = '\0';
-		LcdShowString(10,0,"VO:");
-		LcdShowString(13,0,str);
-		
-		//差分输入模式3,自动转换,因为是读取的上次转换的结果,所以读取的是1通道
-		retval = getADCAutoValue(DIFF3,1); 
-		str[0] = retval / 100 % 10 + '0';
-		str[1] = retval / 10 % 10 + '0';
-		str[2] = retval     % 10 + '0';
-		str[3] = '\0';
-		LcdShowString(0,0,"CHA0:");
-		LcdShowString(6,0,str);
-		
-		//差分输入模式3,自动转换,因为是读取的上次转换的结果,所以读取的是0通道
-		retval = getADCAutoValue(DIFF3,0);  
-		str[0] = retval / 100 % 10 + '0';
-		str[1] = retval / 10 % 10 + '0';
-		str[2] = retval     % 10 + '0';
-		str[3] = '\0';
-		LcdShowString(0,1,"CHA1:");
-		LcdShowString(6,1,str);
-		
-		val ++;
-		delay10usValue(50000);
+			updateTIMER0();
 	}
 }
